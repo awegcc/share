4882b6cc176726b71c331efa17ce0515eae0fd1b
ccshare <chen.cheng@myshare.io>
2018-04-25 18:52:27 +0800

    refine

--- inject-data.sh
+++ inject-data.sh
@@ -6,14 +6,37 @@ ip_port=$ip:$port
 
 echo $ip_port
 
-# output: {"token":"d4bfcbe9756f1f2a1cc5c8867a107f4f5e45e0887293ec7d"}
-curl -XPOST "http://${ip_port}/token?entryKey=$1&entryOp=put" -o .token
-eval $(awk -F\" '{printf("token=%s\n",$4)}' .token)
-echo $token
-curl -v -XPOST "http://${ip_port}/pblocks/$1?token=$token" -H "Content-Type: application/octet-stream" --data-binary @/tmp/$1
+mkdir -p src dst tmp
 
-curl -XPOST "http://${ip_port}/token?entryKey=$1&entryOp=get" -o .token2
-eval $(awk -F\" '{printf("token=%s\n",$4)}' .token2)
-echo $token
-curl -v -XGET "http://${ip_port}/pblocks/$1?token=$token" -o $1
+function upload()
+{
+    # output: {"token":"d4bfcbe9756f1f2a1cc5c8867a107f4f5e45e0887293ec7d"}
+    curl -s -XPOST "http://${ip_port}/token?entryKey=$1&entryOp=put" -o tmp/token
+    eval $(awk -F\" '{printf("token=%s\n",$4)}' tmp/token)
+    curl -s -XPOST "http://${ip_port}/pblocks/$1?token=$token" -H "Content-Type: application/octet-stream" --data-binary @src/$1
+}
 
+function download()
+{
+    curl -s -XPOST "http://${ip_port}/token?entryKey=$1&entryOp=get" -o tmp/token2
+    eval $(awk -F\" '{printf("token=%s\n",$4)}' tmp/token2)
+    curl -s -XGET "http://${ip_port}/pblocks/$1?token=$token" -o dst/$1
+}
+
+for((i=0;i<99999999;))
+do
+    filename=$i
+    echo $i >> src/$filename
+    upload $filename
+    download $filename
+    if diff src/$filename dst/$filename > /dev/null
+    then
+        echo "file $i ok"
+        rm -f dst/$filename
+    else
+	echo "file $i failed"
+        exit
+    fi
+    i=$((i+1))
+    mv src/$filename src/$i
+done
