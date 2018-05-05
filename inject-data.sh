8ceec76d2b4089a434f7a2aeb39c50e6a8fef87a
ccshare <chen.cheng@myshare.io>
2018-05-05 12:53:40 +0800

    Add parameter( -n count )

--- inject-data.sh
+++ inject-data.sh
@@ -3,20 +3,22 @@
 function print_usage()
 {
     local base_name=$(basename $0)
-    echo -e "\033[0;31m Usage: \033[0m $base_name [options...]"
-    echo -e " -h host  Host address(default 127.0.0.1)"
-    echo -e " -p port  port(default 3000)"
-    echo -e " -k key   key prefix(default k)"
-    echo -e " -o options  curl options"
+    echo -e "\033[0;31m  Usage: \033[0m $base_name [options...]"
+    echo -e " -h host    Host address(default 127.0.0.1)"
+    echo -e " -p port    port(default 3000)"
+    echo -e " -k key     key prefix(default k)"
+    echo -e " -n times   inject and download times(default 8192)"
+    echo -e " -o options curl options"
     exit 1
 }
 
 host=127.0.0.1
 port=3000
 prefix='k'
+count=8192
 options='-s'
 
-while getopts ':h:p:k:f:o:' opt
+while getopts ':h:p:k:n:o:' opt
 do
     case $opt in
     h) host=$OPTARG
@@ -24,6 +26,8 @@ do
     p) port="${OPTARG}"
     ;;
     k) prefix="${OPTARG}"
+    ;;
+    n) count="${OPTARG}"
     ;;
     o) options="-$OPTARG"
     ;;
@@ -54,7 +58,7 @@ function download()
     curl -s -XGET "http://${host_port}/pblocks/$key?token=$token" -o dst/$filename
 }
 
-for((i=0;i<9999999999;))
+for((i=0;i<count;))
 do
     filename="${pkey}_$i"
     echo "test file content($filename), and number is: $i" >> src/$filename
