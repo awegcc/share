77b2c782efc0d58d3bf14414e5f1b533fe7edec2
ccshare <chen.cheng@myshare.io>
2018-05-13 22:36:06 +0800

    M

--- pack/webpack.config.js
+++ pack/webpack.config.js
@@ -1,19 +1,24 @@
-
 const path=require('path');
 
 module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath:'',
     filename: 'bundle.js'
   },
   module: {
-    //noParse: /levelup\.js$/
+    rules: [
+      {
+        test: /\.node$/,
+        loader: 'node-native'
+      }
+    ]
   },
   externals: {
     'leveldown':"require('leveldown')",
-    'secp256k1':"require('secp256k1')",
-    'bindings':"require(''bindings'')"
+    'secp256k1':"require('secp256k1')"
+    'bindings':"require('bindings')"
   },
   target: 'node'
 };
