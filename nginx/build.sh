a53aade3f48b7b51e9233854c64cfbad0127433b
ccshare <chen.cheng@myshare.io>
2018-09-16 00:25:26 +0800

    refine

--- nginx/build.sh
+++ nginx/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 NAME=mynginx
-VERSION=1.0.0-22-eeeff
+VERSION=1.0.0-22-000001
 
-echo "Building image ($VERSION) ..."
+echo "Building image ($NAME:$VERSION) ..."
 docker build -t $NAME:$VERSION -f Dockerfile.nginx .
 docker tag $NAME:$VERSION $NAME:latest
+
