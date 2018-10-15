1fa0a82a1765b5cffe020ac1bd813001320bc6d5
ccshare <chen.cheng@myshare.io>
2018-10-15 18:46:05 +0800

    Tweak

--- block-cache.js
+++ block-cache.js
@@ -1,19 +1,27 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = requier('path');
+const Cache = require('streaming-cache');
 
 const CACHE_SIZE = 64
 const CACHE_MAX_AGE = 4 * 24 * 3600
 const CACHE_DIR = "/tmp/"
+const CACHE_THRESHOLD = 64 * 1024
 
 class BlockCache {
     constructor(opts = {}) {
         this.size = CACHE_SIZE;
         this.age = CACHE_MAX_AGE;
         this.dir = opts.dir || CACHE_DIR;
+
+        let cache_options = {
+            max: CACHE_SIZE,
+            maxAge: CACHE_MAX_AGE
+        };
+        this.mcache = new Cache(cache_options);
     }
 
     exists(key) {
-        return fs.existsSync(path.join(this.dir, key));
+        return this.mcache.exists(key) || this.fs.existsSync(path.join(this.dir, key));
     }
 
     delete(key) {
@@ -36,21 +44,30 @@ class BlockCache {
 
     createReadStream(key) {
         let rs = null;
-        try {
-            rs = fs.createReadStream(path.json(this.dir, key))
-        } catch (err) {
-            rs = null
+        rs = this.cache.get(key)
+        if (!rs) {
+            try {
+                rs = fs.createReadStream(path.json(this.dir, key))
+            } catch (err) {
+                rs = null
+            }
         }
         return rs;
     }
 
-    createWriteStream(key) {
+    createWriteStream(key, size) {
         let ws = null;
-        if (!this.exists(key)) {
-            try {
-                ws = fs.createWriteStream(path.json(this.dir, key))
-            } catch (err) {
-                ws = null
+        if (size) {
+            if (!this.exists(key)) {
+                if (size < CACHE_THRESHOLD) {
+                    ws = this.mcache.set(key)
+                } else {
+                    try {
+                        ws = fs.createWriteStream(path.json(this.dir, key))
+                    } catch (err) {
+                        ws = null
+                    }
+                }
             }
         }
         return ws;
