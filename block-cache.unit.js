b866dc3a3a4c723d77367cf3ade6ad959ff9d2b3
ccshare <chen.cheng@myshare.io>
2018-10-15 18:50:43 +0800

    Add test case

---
+++ block-cache.unit.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+describe('client/block-cache', () => {
+  //
+  descrite('@constructor', () => {
+    console.log('success');
+  });
+
+});
