00a6f697bff7721fc3e8bc0f26c0060ee644f01f
cchare <admin@aweg.cc>
2018-10-25 22:35:28 +0800

    Add nginx config file

--- nginx/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NAME=mynginx
-VERSION=1.0.0-22-000001
-
-echo "Building image ($NAME:$VERSION) ..."
-docker build -t $NAME:$VERSION -f Dockerfile.nginx .
-docker tag $NAME:$VERSION $NAME:latest
-
